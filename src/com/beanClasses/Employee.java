9682a9a6666140bc2e484b508f1e9054bdd9fff1
GaneshGanta <ganeshsagar373@gmail.com>
2022-09-29 22:28:35 +0530

    day2

--- src/com/beanClasses/Employee.java
+++ src/com/beanClasses/Employee.java
@@ -5,7 +5,6 @@ public class Employee {
 	
 	private int employeeId;
 	private String employeeName;
-	private int wage;
 	private int days;
 	
 	public int getEmployeeId() {
@@ -20,12 +19,7 @@ public class Employee {
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
-	public int getWage() {
-		return wage;
-	}
-	public void setWage(int wage) {
-		this.wage = wage;
-	}
+	
 	public int getDays() {
 		return days;
 	}
@@ -34,7 +28,7 @@ public class Employee {
 	}
 	@Override
 	public String toString() {
-		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", wage=" + wage + ", days="
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", days="
 				+ days + "]";
 	}
 	public Employee(String employeeName ) { 
