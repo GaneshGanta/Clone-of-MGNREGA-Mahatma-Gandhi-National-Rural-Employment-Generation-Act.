16869b8adfac7b1afd06b9e70e76cfe1ba244db6
GaneshGanta <ganeshsagar373@gmail.com>
2022-09-29 22:12:23 +0530

    day2

--- src/com/beanClasses/Project.java
+++ src/com/beanClasses/Project.java
@@ -7,6 +7,21 @@ public class Project {
 	private int projectId;
 	private String  projectName;
 	private int budjget;
+	private int dailyWage;
+	
+	
+	public Project(String projectName, int budjget, int dailyWage) {
+		super();
+		this.projectName = projectName;
+		this.budjget = budjget;
+		this.dailyWage = dailyWage;
+	}
+	public int getDailyWage() {
+		return dailyWage;
+	}
+	public void setDailyWage(int dailyWage) {
+		this.dailyWage = dailyWage;
+	}
 	public int getProjectId() {
 		return projectId;
 	}
@@ -29,11 +44,7 @@ public class Project {
 	public String toString() {
 		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", budjget=" + budjget + "]";
 	}
-	public Project(String projectName, int budjget) {
-		super();
-		this.projectName = projectName;
-		this.budjget = budjget;
-	}
+	
 	public Project() {
 		super();
 	}
